4135d2f4ac5f64a32dd23f76e54b807ad7c0a58b
Michał Bryk <galeriakoloru90210@gmail.com>
2023-07-04 23:12:14 +0200

    implement delete modal

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -15,6 +15,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { AlertModal } from "@/components/modals/AlertModal";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -52,6 +53,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
 
   return (
     <>
+      <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={() => {}} loading={loading} />
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
         <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
