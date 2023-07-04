a20065fb9e0f57d1b1213957850c84d7ff555abd
Michał Bryk <galeriakoloru90210@gmail.com>
2023-07-04 21:49:32 +0200

    finish form in settings form

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,16 +1,41 @@
 "use client";
 
+import * as z from "zod";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
 import { Heading } from "@/components/ui/Heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Store } from "@prisma/client";
-import { Trash } from "lucide-react";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -20,6 +45,28 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input disabled={loading} placeholder="Store name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
