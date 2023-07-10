10aa299485ac2efe3f4c06f1ef9d0ad11d72f160
Michał Bryk <galeriakoloru90210@gmail.com>
2023-07-10 22:42:42 +0200

    configure cloudinary upload to banners

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -18,6 +18,7 @@ import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/AlertModal";
 import { ApiAlert } from "@/components/ui/ApiAlert";
 import { useOrigin } from "@/hooks/use-origin";
+import ImageUpload from "@/components/ui/ImageUpload";
 
 interface BillboardFormProps {
   initialData: Bilboard | null;
@@ -97,6 +98,24 @@ const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
       <Separator />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background Image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
