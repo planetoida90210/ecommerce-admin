59874497c1625a0b6cec6c27bace4346cbbcf523
Michał Bryk <galeriakoloru90210@gmail.com>
2023-08-01 23:37:50 +0200

    finish products GET and POST

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -57,17 +57,29 @@ export async function POST(
             return new NextResponse("Unauthorized", {status: 403})
         }
 
-        const billboard = await prismadb.bilboard.create({
+        const product = await prismadb.product.create({
             data:{
-                label,
-                imageUrl,
-                storeId: params.storeId
+                name,
+                price,
+                isFeatured,
+                isArchived,
+                categoryId,
+                colorId,
+                sizeId,
+                storeId: params.storeId,
+                images: {
+                    createMany: {
+                        data: [
+                            ...images.map((image: {url: string}) => image)
+                        ]
+                    }
+                }
             }
         })
 
-        return NextResponse.json(billboard);
+        return NextResponse.json(product);
     } catch (error) {
-        console.log('[BILLBOARDS_POST]', error);
+        console.log('[PRODUCTS_POST]', error);
         return new NextResponse('Internal error', {status: 500})
     }
 }
@@ -77,17 +89,38 @@ export async function GET(
     { params }: {params: {storeId: string}}
 ) {
     try {
+        const {searchParams} = new URL(req.url);
+        const categoryId = searchParams.get("categoryId") || undefined
+        const colorId = searchParams.get("colorId") || undefined
+        const sizeId = searchParams.get("sizeId") || undefined
+        const isFeatured = searchParams.get("isFeatured") 
+    
+
         if(!params.storeId) {
             return new NextResponse("Store ID is required", {status: 400})
         }
 
-        const billboards = await prismadb.bilboard.findMany({
+        const products = await prismadb.product.findMany({
             where: {
-                storeId: params.storeId
+                storeId: params.storeId,
+                categoryId,
+                colorId,
+                sizeId,
+                isFeatured: isFeatured ? true : undefined,
+                isArchived: false
+            },
+            include: {
+                images: true,
+                category: true,
+                color: true,
+                size: true
+            },
+            orderBy: {
+                createdAt: 'desc'
             }
         })
 
-        return NextResponse.json(billboards);
+        return NextResponse.json(products);
     } catch (error) {
         console.log('[BILLBOARDS_GET]', error);
         return new NextResponse('Internal error', {status: 500})
